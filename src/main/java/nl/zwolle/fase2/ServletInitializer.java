35757db7e0d082efe6ebe121f67f699c32d4bcde
FelixvL <digitaleservice@gmail.com>
2017-10-11 23:00:28 +0200

    commit 1 war

---
+++ src/main/java/nl/zwolle/fase2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package nl.zwolle.fase2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Fase2Application.class);
+	}
+
+}
