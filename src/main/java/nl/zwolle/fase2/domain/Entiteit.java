46c29cfaf31a062ae9d1439f2a5d6fab1a036054
FelixvL <digitaleservice@gmail.com>
2017-10-11 23:09:50 +0200

    Met entiteit

---
+++ src/main/java/nl/zwolle/fase2/domain/Entiteit.java
@@ -0,0 +1,36 @@
+package nl.zwolle.fase2.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
+public class Entiteit {
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	private String naam;
+	private int getal;
+	
+	public Entiteit(String naam){
+		this.naam = naam;
+	}
+	public Entiteit() {}
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNaam() {
+		return naam;
+	}
+
+	public void setNaam(String naam) {
+		this.naam = naam;
+	}
+}
